eee1e8850818b07b9373a7bf43da016080b1ce91
Sudip Manna <sudip.manna2024@uem.edu.in>
2026-03-11 21:08:27 +0530

    Given a sorted array and a number X, search two elements such that their sum is X.

---
+++ DAA_Q1.java
@@ -0,0 +1,16 @@
+arr = [1, 2, 4, 7, 11, 15]
+X = 15
+
+left, right = 0, len(arr) - 1
+
+while left < right:
+    s = arr[left] + arr[right]
+    if s == X:
+        print("Elements are", arr[left], "and", arr[right])
+        break
+    elif s < X:
+        left += 1
+    else:
+        right -= 1
+else:
+    print("No such pair found")
