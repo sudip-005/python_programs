c47ecaad652462ac970f28b9ef7fa9201a539408
Sudip Manna <sudip.manna2024@uem.edu.in>
2026-03-26 22:16:46 +0530

    Update Q5.py

--- DAA/Q5.py
+++ DAA/Q5.py
@@ -1,18 +1,14 @@
 def merge_sort(arr):
     if len(arr) <= 1:
         return arr, 0
-
     mid = len(arr) // 2
     left, inv_left = merge_sort(arr[:mid])
     right, inv_right = merge_sort(arr[mid:])
-
     merged, inv_split = merge(left, right)
     return merged, inv_left + inv_right + inv_split
-
 def merge(left, right):
     i = j = inv = 0
     merged = []
-
     while i < len(left) and j < len(right):
         if left[i] <= right[j]:
             merged.append(left[i])
@@ -21,12 +17,9 @@ def merge(left, right):
             merged.append(right[j])
             inv += len(left) - i
             j += 1
-
     merged.extend(left[i:])
     merged.extend(right[j:])
     return merged, inv
-
 arr = [2, 4, 1, 3, 5]
 _, count = merge_sort(arr)
-
 print("Number of inversions =", count)
