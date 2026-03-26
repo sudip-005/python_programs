06a023d4a29bd48a39a51b5c2089c495d6d0103e
Sudip Manna <sudip.manna2024@uem.edu.in>
2026-03-26 22:15:45 +0530

    Update Q11.py

--- DAA/Q11.py
+++ DAA/Q11.py
@@ -7,7 +7,6 @@ def partition(a, l, r):
             i += 1
     a[i], a[r] = a[r], a[i]
     return i
-
 def quick_select(a, l, r, k):
     if l <= r:
         p = partition(a, l, r)
@@ -17,12 +16,9 @@ def quick_select(a, l, r, k):
             return quick_select(a, l, p-1, k)
         else:
             return quick_select(a, p+1, r, k)
-
 arr = [7,2,1,6,8,5,3]
 n = len(arr)
 m = n//2
-
 idx = quick_select(arr, 0, n-1, m)
-
 print("Median =", arr[idx])
 print("Neighbors =", arr[idx-1], "and", arr[idx+1])
